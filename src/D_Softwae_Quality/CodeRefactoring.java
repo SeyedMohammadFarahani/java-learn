17dd2816e5994059d813e223c9ea56ccbc3679c2
mohammad farahani <s.mohamad.farahani@gmail.com>
2023-03-18 00:33:41 +0330

    add Code Refactoring

---
+++ src/D_Softwae_Quality/CodeRefactoring.java
@@ -0,0 +1,24 @@
+package D_Softwae_Quality;
+
+public class CodeRefactoring {
+
+    /*Todo Refactoring technique */
+    // 1) Bad Names
+    // Change Names
+    // 2) Duplicate Code
+    // extract variables
+    // extract methods
+    // extract classes
+    // 3) Long Method
+    // extract methods
+    // 4) Large Class
+    // extract subclass or super class
+    // 5) Too many parameters
+    // Replace Parameter with Method Call
+    // 6) Feature Envy (متد/متدهایی که از بیشتر از طرف یک کلاس دیگر فراخوانی می‌شود یا کلاسی که متدهای کلاسی دیگر را بیش از حد فراخوانی می‌کند)
+    // Move Method
+
+
+
+
+}
